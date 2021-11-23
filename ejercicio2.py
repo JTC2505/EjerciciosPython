d30a5914deed7dd908b1cc42b459e1eb3b7be1b3
J. Estuardo Telguario <estuardotelejt@gmail.com>
2021-11-22 19:21:19 -0600

    Ejercicio 2 - Completo

---
+++ ejercicio2.py
@@ -0,0 +1,22 @@
+from ejercicio2Modulo import *
+
+continuar = 's'
+while continuar == 's' or continuar == 'S':
+    print('\nVentas realizadas(cantidades): ')
+    desayunos = float(input('Total desayunos: '))
+    almuerzos = float(input('Total almuerzos: '))
+    cenas = float(input('Total cenas: '))
+
+    print('\nPrecios tiempos: ')
+    desayunosSub = float(input('Por favor ingrese el valor del desayuno: '))
+    almuerzosSub = float(input('Por favor ingrese el valor del almuerzo: '))
+    cenasSub = float(input('Por favor ingrese el valor de la cena: '))
+
+    print('Estos son los resultados: ')
+    print(f'Total desayunos: {totalTiempo(desayunos, desayunosSub)}')
+    print(f'Total almuerzos: {totalTiempo(almuerzos, almuerzosSub)}')
+    print(f'Total cenas: {totalTiempo(cenas, cenasSub)}')
+    print(f'TOTAL: {sumaTotal(totalTiempo(desayunos, desayunosSub), totalTiempo(almuerzos, almuerzosSub), totalTiempo(cenas, cenasSub))}')
+
+    print('\ndesea hacer este proceso otra vez? (s/n)')
+    continuar = input()
---
+++ ejercicio2Modulo.py
@@ -0,0 +1,4 @@
+def totalTiempo(venta, subtotal):
+    return round(venta * subtotal, 2)
+def sumaTotal(desayunos, almuerzos, cenas):
+    return round(desayunos + almuerzos + cenas , 2)
